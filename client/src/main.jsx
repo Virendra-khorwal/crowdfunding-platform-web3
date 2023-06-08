7ca6342953e541adddd14881828906c30708f36d
Virendra Khorwal <virendrakhorwalvk@gmail.com>
2023-06-08 19:29:47 +0530

    asset add tailwind config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { Sepolia } from "@thirdweb-dev/chains";
+
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+
+import App from "./App";
+import './index.css';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+// const activeChain = "ethereum";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
+    <ThirdwebProvider activeChain={Sepolia}>
+      <Router>
+        <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
